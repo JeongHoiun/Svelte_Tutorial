f300c7940f0a5a73811323ebcee0b7c91cacd6f6
hoiunjeong <un4255@gmail.com>
2022-10-10 21:32:40 +0900

    chap 2. reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	export let name: string;
+	import ReactivityButton from './chap2/Reactivity.svelte';
+	let name: string;
+	name = 'me'
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<ReactivityButton />
 </main>
 
 <style>
@@ -17,7 +19,6 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
---
+++ src/chap2/Reactivity.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+	let count = 0;
+
+	function incrementCount() {
+		count += 1
+	}
+</script>
+
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 || count === 0 ? 'time' : 'times'}
+</button>
