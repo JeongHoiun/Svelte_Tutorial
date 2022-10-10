2912e6f414daa8abffa19eb6f78b9a2d937ba909
hoiunjeong <un4255@gmail.com>
2022-10-10 21:43:24 +0900

    chap2 Declarations

--- src/chap2/Reactivity.svelte
+++ src/chap2/Reactivity.svelte
@@ -1,11 +1,15 @@
-<script lang='ts'>
+<script>
 	let count = 0;
+	$: doubled = count * 2;
 
-	function incrementCount() {
-		count += 1
+	function handleClick() {
+		count += 1;
 	}
 </script>
 
-<button on:click={incrementCount}>
-	Clicked {count} {count === 1 || count === 0 ? 'time' : 'times'}
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
+<p>
+	{count} doubeld is {doubled}
+</p>
